23c6e7445a2367f703698a487485f5287510e5e6
cwpark <cwpark@bbros.kr>
2022-10-17 01:20:07 +0900

    대화상자

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     private var _score = 0
+    val score: Int
+        get() = _score
+
     private var _currentWordCount = 0
     val currentWordCount: Int
         get() = _currentWordCount
